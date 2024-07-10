11652e713001aada572d92e10d1dbe96fd4931cb
Maksim Dav <maksim.dav@mail.ru>
2024-07-10 11:09:31 +0800

    swap BrowserRouter for HashRouter, fix name in package.json

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { Provider } from 'react-redux';
 import { store } from './services/store/store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
